cc355faf244f41b1a44b4da935e78dfdfc1f9754
Subhashganni <113432507+Subhashganni@users.noreply.github.com>
2022-10-22 21:46:58 +0530

    Add files via upload

---
+++ migrations/4_initial_migration.js
@@ -0,0 +1,5 @@
+const Forwarder = artifacts.require("Forwarder");
+
+module.exports = function (deployer) {
+  deployer.deploy(Forwarder,"0x4aBca7dccB3652b9B43a0F5753B8E1c77fDBD427");
+};
---
+++ migrations/5_initial_migration.js
@@ -0,0 +1,5 @@
+const clearence = artifacts.require("clearence");
+
+module.exports = function (deployer) {
+  deployer.deploy(clearence," 0x0F05EdF1BDDA21A1CD72A643e59230aD9Ae4F2Ba");
+};
